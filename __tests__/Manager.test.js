11ae4777997dbf3e136ee63fc57327476a8bf07b
ryanjolley12 <ryanswims@gmail.com>
2021-07-26 14:49:50 -0400

    added manager test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = reequire("@jest/globals");
+const Manager = require("../lib/Manager");
+
+test("create new manager", () => {
+    const manager = new Manager("Ryan");
+
+    expect(manager).toBeInstanceOf(Manager)
+    expect(manager.user).toBe("Ryan");
+    expect(manager.id).toEqual(expect.any(String);
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.number).toEqual(expect.any(String));
+
+});
+
+test("check manager functions", () => {
+    const manager = new Manager("Ryan");
+
+    expect(manager.getRole()).toBe("Manager");
+    expect(manager.getEmail()).toBeDefined();
+    expect(manager.getId()).toBeDefined();
+    expect(manager.getName()).toBeDefined();
+});
