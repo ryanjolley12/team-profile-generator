875c5eaa74c021311025bfbf01044ccedc699db3
ryanjolley12 <ryanswims@gmail.com>
2021-07-26 14:38:35 -0400

    intern test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+test('test new Intern', () => {
+    const intern = new Intern('Colby');
+    expect(intern.user).toBe('Colby');
+    expect(intern).toBeInstanceOf(Intern)
+})
+
+test("check intern functions", () => {
+    const intern = new Intern("Ryan");
+
+    expect(intern.getRole()).toBe("Intern");
+    expect(intern.getEmail()).toBeDefined();
+    expect(intern.getId()).toBeDefined();
+    expect(intern.getName()).toBeDefined();
+    expect(intern.getSchool()).toBeDefined();
+});
